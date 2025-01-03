f32f9052d370743ed758cdf3706ff99963aca174
venky <venkatvenkat26483@gmail.com>
2025-01-03 16:21:44 +0530

    pdf set up

---
+++ resources/views/40MonthsAgreement.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Generate PDF using Laravel TCPDF</title>
+    <style>
+      .bottom-margin
+      {
+        border-bottom:1px solid #000;
+      }
+      .gap
+      {
+        margin-bottom:2rem !important;
+      }
+    </style>
+</head>
+<body >
+    <h1 style="color:red;" align="center">Agreement</h1>
+     <div>
+        <p style="font-size:16rem; font-weight:300;" align="center" >40 MONTHS SCHEME AGREEMENT: VIINDHYA BULLION PVT. LTD.</p>
+        <p style="font-weight:300">
+            <span style="font-size:13rem; font-weight:300">40 MONTHS SCHEME</span> Agreement for Business Alliances made and executed on this the <span style="background-color:yellow">16th day of August 2024, (1st day of May 2024,)</span>  at Hyderabad by and between:
+        </p>
+        <p style="font-weight:300">
+            <span style="font-size:13rem; font-weight:300">VIINDHYA BULLION AU PVT. LTD.</span>, Rep. by its Managing Director, <span style="font-size:13rem; font-weight:300">CH. ANJAN PRASAD</span>, age: 42 years, O/o. # 8-3-191/92/SF (71/A), 2nd Floor, Surya Plaza, Vengal Rao Nagar, Hyderabad, Telangana – 500038. Adhar No. 4220 0624 7310.
+        </P>
+        <p style="margin-left:5rem">(Herein after called as “<span style="color:red">FIRST PARTY</span>”).</p>
+        <div align="end">AND</div>
+        <p style="font-weight:300; background-color:yellow; margin-left:5rem; margin-bottom:2rem"  >
+            Name: GUDA DILIP REDDY S/O GUDA LAXMA REDDY, AGE: 41 YEARS, ADDRESS: FLAT NO 304, 3RD FLOOR, JYOTHI PRIDE APPTS, SPRING VALLEY, KONDAPUR, HYDERABAD, TELANGANA- 500084, Adhar No: 7208 7360 7108
+        </p>
+        <p justify-content="center">(Herein after called as “<span style="color:red">SECOND PARTY</span>”).</p>
+        <p style="font-weight:300">
+            <span style="font-size:13rem; font-weight:300">FIRST PARTY</span> is an Indian conglomerate encircling numerous prosperous trades created with innovation at its core by means of trust amongst its customers spread all around the country with major stake of customers from South India. <spn style="font-size:13rem; font-weight:300">FIRST PARTY</span> has cherished Bullion business space, creating exhilarating yet challenging opportunities with the frame work of conducive strategies, quality products with efficient services, by maintaining ethical values with well-organized approach to address the demand and supply chain is manifestation of the commitment to vivid and transparent correspondences with all the stake holders catering a plethora of services to the wider customer range all across India.
+        </p>
+        <p style="font-weight:300">
+            <span style="font-size:13rem; font-weight:300">FIRST PARTY</span> emphasis is not only on profit out of the trading but also to establish and encourage the activity that transforms wellbeing and prosperity for all the customers as well as those who are directly or indirectly connected with them, where the word <span style="font-size:13rem; font-weight:300">“PROFIT”</span> becomes only a by-product of what they all do as entirety. <span style="font-size:13rem; font-weight:300">VIINDHYA BULLION PVT LTD</span> is one of the active Bullion dealers in the country having vast network of customer base in multi-fold. They sell and trade 999 purity Gold Bars/ Ingots with the best quality gold certified by the Authorities recognized by the Govt. of India.
+        </p>
+        <p style="font-weight:300">
+            <span style="font-size:13rem; font-weight:300">SECOND PARTY</span> is the transferor in the <span style="font-size:13rem; font-weight:300">FIRST PARTY</span> business with the terms and conditions as cited below.
+        </p>
+     </div>
+     <div>
+        <div style="font-size:15rem; font-weight:300">
+            TERMS AND CONDITIONS:
+        </div>
+        <ol style="font-weight:300;">
+            <div>
+                <li style="margin-bottom:2rem" class="<?='gap'?>">
+                    The present agreement is acceptance by both the parties stated above for <span style="font-size:13rem; font-weight:300">SECOND PARTY’</span>s purchase of gold in the <span style="font-size:13rem; font-weight:300">FIRST PARTY</span> business, more specifically gold business which is conducted throughout India depending upon the customer’s demand.
+                </li>
+            </div>
+            <div>
+                <li>
+                    The <span style="font-size:13rem; font-weight:300">FIRST PARTY</span> accepts transfer <span style="background-color:yellow">of Rs.10,00,000/-(Rupees Ten Lakhs Only)</span> in their business from the Second Party. The <span style="font-size:13rem; font-weight:300">FIRST PARTY</span> authorizes and permits the <span style="font-size:13rem; font-weight:300">SECOND PARTY</span> for agreed amount as stated below in the third point in the terms and conditions.
+                </li>
+            </div>
+            <div>
+                <li>
+                    The terms of business conducted in between the <span style="font-size:13rem; font-weight:300">FIRST PARTY</span> and the <span style="font-size:13rem; font-weight:300">SECOND PARTY</span> shall be of <span style="font-size:13rem; font-weight:300">BUSINESS ALLIANCE</span> and the <span style="font-size:13rem; font-weight:300">FIRST PARTY</span> accepts the sum of <span style="background-color:yellow">Rs. 10,00,000/- (Rupees Ten Lakhs Only)</span> from <span style="font-size:13rem; font-weight:300">SECOND PARTY</span> and further the SECOND PARTY is benefitted with double the amount that is <span style="background-color:yellow">Rs. 20,00,000/- (Rupees Twenty Lakhs Only)</span> which shall be divided into 40 equal Equated Monthly Installments. The monthly EMI for the total amount of <span style="background-color:yellow">Rs. 20,00,000/-</span> for the next 40 months is <span style="background-color:yellow">Rs. 50,000/- (Rupees Fifty Thousand Only)</span> per month. The EMIs value will be considered to allow the First Party to give equal value of gold to the Second Party every month.
+                </li>
+            </div>
+            <div>
+                <li>
+                    Price of the gold will be fixed by the first party in the first five working days of the month by considering the screen rate displayed in <a routerLink="">www.vibullion.com/</a> in between 12. P.M., & 4 P.M., and the same shall be informed to the Second Party vide Text Message to the registered mobile number of the Second Party with the First Party and from then within 24 hours the gold coin/bar (99.9 purity) shall be kept ready to be delivered from First Party’s head office or production plant. The second party can any time pickup the EMI in gold form from the head office or production plant of the First Party from thereon. If the Second Party wishes to get the gold coins/bars delivered to their location, they must bear extra courier/freight charges. The gold bars/coins that are to be given as EMI to the Second Party comes with 3% GST in accordance with the government norms. No Making Charges and No wastage are added to such gold coins/bars.
+                </li>
+            </div>
+            <div>
+                <li>
+                    The scheme is bound to be continued for 40 months specified as above and so the transferred amount shall not be returned within the 40 months for any reason.
+                </li>
+            </div>
+            <div>
+                <li>
+                    If the amount transferred by the Second Party in the First Party is on or before 5th of a month, then the subsequent month will be considered for repayment of EMI in the form of gold and the Second Party shall receive EMI in the form of gold by next month 5th working day.
+                </li>
+            </div>
+            <div>
+                <li>
+                    If the amount transferred by the Second Party in the First Party is after 5th of a month, then the Second party will receive Rs.2.50/- (Rupee Two and Fifty Paisa) as interest towards the total transferred amount for the number of days. This interest amount shall be clubbed along with the next month’s EMI amount and calculated with grand total and the gold equated to the grand total amount will be given to the Second Party by the First Party in the procedure stated above in the clause no.4.
+                </li>
+            </div>
+            <div>
+                <li>
+                    In the case of amount transfer being more than <span style="background-color:Yellow">Rs. 25,00,000/- (Rupees Twenty-Five Lakhs Only)</span>, a jewel card is provided by the First Party to the Second Party. The jewel card validity shall be for a year. With the use of the jewel card, if any one referred by second party purchases gold from the first party, the second party will get 0.5% commission over the value of purchase made by the referred person and further 2% discount will be given on the bill purchased. The commission will be given to the Second Party after completion of that month by computing all such referral purchases with 10%TDS deduction.
+                </li>
+            </div>
+            <div>
+                <li>
+                    The Second Party has every right concluded and agreed upon in the present agreement; but other than the Second Party, none including the family members or friends of the Second Party has any right in the business and also, they do not have even right to learn the business status or any other information pertaining to the present business discussed in the current agreement.
+                </li>
+            </div>
+            <div>
+                <li>
+                    The amount is accepted through the bank transfer from Second Party to the First Party and it is accepted by both the parties that the return of the amount in total shall be returned only in the form of GOLD (99.9 Purity). It is always open for both the parties to discuss and further increase the amount flow by the Second Party into the present sequence of gold business.
+                </li>
+            </div>
+            <div>
+                <li>
+                    The present agreement deals with the gold business only.
+                </li>
+            </div>
+            <div>
+                <li>
+                    Tenure of the current agreement is for Forty Months starting from the date of this agreement. No renewal possible for the present agreement.
+                </li>
+            </div>
+            <div>
+                <li>
+                    As a security Yes Bank Cheque bearing No. <span style="background-color:yellow; color:red;">846821</span> for Rs. 10,00,000/- (Rupees Ten Lakhs Only) is issued by the First Party in favor of the Second Party as security
+                </li>
+            </div>
+            <div>
+                <p style="font-size:15rem; font-weight:300">AMOUNT TRANSFER SUMMARY:</p>
+                <ul>
+                <li>
+                    <span style="font-weight:300">Details of the Bank A/c:<span>  <span style="font-weight:100;">IDFC BANK</span>
+                </li>
+                <li>
+                    <span style="font-weight:300">Branch Location & City:</span> <span>HI TECH CITY, HYDERABAD</span>
+                </li>
+                <li>
+                    <span style="font-weight:300">Name:</span> <span>GUDA DILIP REDDY</span>
+                </li>
+                <li>
+                    <spa style="font-weight:300"n>A/c No:</span> <span style="font-weight:100;">10137630625</span>          <span style="font-weight:300">IFSC Code:</span> <span style="font-weight:100;">IDFB0080222</span>
+                </li>
+                <ul>
+            </div>
+        </ol>
+     </div>
+     <div>
+        <div style="font-weight:300;">
+            <p style="font-size:15rem; font-weight:300">AMOUNT TRANSFER FLOW:</p>
+            <ul>
+                <li>BANK NAME : YES BANK</li>
+                <li>CHEQU NUMBER : <span style="background-color:yellow; color:red;">846821</span></li>
+                <li>BANK HOLDER NAME : VIINDHYA AU BULLION LLP</li>
+                <li>ACCOUNT NUMBER : 000627000000342</li>
+                <li>IFS CODE : YESB0000006</li>
+                <li>BRANCH NAME : SOMAJIGUDA</li>
+            </ul>
+        </div>
+     </div>
+</body>
+</html>
